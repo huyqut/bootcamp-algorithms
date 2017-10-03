3d39b9891eb3dc94a3a4aa5548d273950ec09cef
huyqut <huy.qut@gmail.com>
2017-10-03 18:14:38 +0700

    Optimize: Space O(n) Longest Common Subsequence

--- main/dynapro/longest_common_subsequence.py
+++ main/dynapro/longest_common_subsequence.py
@@ -5,25 +5,20 @@ def longest_common_subsequence(a: str, b: str) -> str:
     if not a or not b:
         return ''
 
-    table = np.array([[''] * len(a)] * len(b), dtype = object)
+    table = np.array([''] * len(a), dtype = object)
     if a[0] == b[0]:
-        table[0][0] = a[0]
+        table[0] = a[0]
     for i in range(1, len(a)):
         if a[i] == b[0]:
-            table[0][i] = b[0]
+            table[i] = b[0]
         else:
-            table[0][i] = table[0][i - 1]
+            table[i] = table[i - 1]
     for i in range(1, len(b)):
-        if a[0] == b[i]:
-            table[i][0] = str(a[0])
-        else:
-            table[i][0] = table[i - 1][0]
-    for i in range(1, len(b)):
+        if b[i] == a[0]:
+            table[0] = a[0]
         for j in range(1, len(a)):
-            if len(table[i - 1][j]) > len(table[i][j - 1]):
-                table[i][j] = table[i - 1][j]
-            else:
-                table[i][j] = table[i][j - 1]
-            if b[i] == a[j] and len(table[i][j]) <= min(i, j):
-                table[i][j] += a[j]
-    return table[len(b) - 1][len(a) - 1]
+            if len(table[j - 1]) > len(table[j]):
+                table[j] = table[j - 1]
+            if b[i] == a[j] and len(table[j]) <= min(i, j):
+                table[j] += a[j]
+    return table[len(a) - 1]
